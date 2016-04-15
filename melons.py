096cb4250b9bb53bd5c96187a051e0da9b88d00a
Hackbright Student <info@hackbrightacademy.com>
2016-04-15 12:33:24 -0700

    Working melon classes

--- melons.py
+++ melons.py
@@ -11,9 +11,17 @@ class AbstractMelonOrder(object):
 
     def get_total(self):
         """Calculate price."""
-
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+
+        if self.species == "Christmas Melon":
+            base_price = base_price * 1.5
+
+        if self.order_type == "international" and self.qty < 10:
+            flat_fee = 3.00
+            total = ((1 + self.tax) * self.qty * base_price) + flat_fee
+        else:
+            total = (1 + self.tax) * self.qty * base_price
+
         return total
 
     def mark_shipped(self):
